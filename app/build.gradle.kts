f565093822b0a526da5338ab88bc898c77b2bb55
omkhan297-lgtm <omkhan297@gmail.com>
2026-08-14 15:28:32 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,26 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
-android { namespace = "com.noorequran.app"; compileSdk = 35
-    defaultConfig { applicationId = "com.noorequran.app"; minSdk = 26; targetSdk = 35; versionCode = 2; versionName = "1.0.0-functional" }
+android {
+    namespace = "com.noorequran.app"
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        applicationId = "com.noorequran.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.0.0-functional"
+    }
 }
